ac0e8bff1b5be18c3bbec273f837273e40db02e1
deadpooltanishq78 <31615081+deadpooltanishq78@users.noreply.github.com>
2017-09-28 08:49:57 +0530

    Update img.py

--- img.py
+++ img.py
@@ -1,12 +1,13 @@
-import os
-import time
+#program to take pictures useing rapberry pie via python programing
+import os #import os libraries
+import time#import time libraries
 a=0
 b=1
-while a<=10:
+while a<=10:#while statement
 
     t="fswebcam -F 5 --fps 20 -r \"1200x800\" /home/pi/cam/imgs/"+str(b)+".jpg"
     os.system(t)
     print(b)
     a=a+1
     b=b+1
-    time.sleep(30)
+    time.sleep(30)# time diffrence
